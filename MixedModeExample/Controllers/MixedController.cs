a046cf02338e61a10e7f0eabdd2d9b10c4ee5d42
Håkan Edling <hakan@tidyui.com>
2014-02-07 15:22:00 +0100

    Initial commit.

---
+++ MixedModeExample/Controllers/MixedController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MixedModeExample.Controllers
+{
+    public class MixedController : Piranha.Mvc.SinglePageController
+    {
+        //
+        // GET: /Mixed/
+
+        public ActionResult Index()
+        {
+			var model = new Models.MixedModel() { 
+				Now = DateTime.Now,
+				CMSData = this.GetModel("mixed")
+			};
+			return View(model);
+        }
+
+    }
+}
---
+++ MixedModeExample/Models/MixedModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MixedModeExample.Models
+{
+	public class MixedModel
+	{
+		public DateTime Now { get; set; }
+		public Piranha.Models.PageModel CMSData { get; set; }
+	}
+}
